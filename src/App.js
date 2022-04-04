0dc7913f5a706b0ebe0cd2ccac44590210260687
Denver McCarthy <dmichaelspdx@gmail.com>
2022-04-04 12:53:38 -0700

    started over, got one piece of state moved in to my context

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import Player from './components/Player';
 import CardList from './components/CardList';
 import { useState } from 'react';
 import ExecutePassButton from './components/ExecutePassButton';
+import { useGameData } from './GameContex/GameContex';
 
 function App() {
   const [deck, setDeck] = useState(initialCards);
-  const [playerOneHand, setPlayerOneHand] = useState([]);
+  // const [playerOneHand, setPlayerOneHand] = useState([]);
   const [selectedCard, setSelectedCard] = useState();
   const [playerTwoHand, setPlayerTwoHand] = useState([]);
   const [playerThreeHand, setPlayerThreeHand] = useState([]);
   const [from, setFrom] = useState('deck');
   const [to, setTo] = useState(1);
+
+  const { playerOneHand, setPlayerOneHand } = useGameData();
 
   function findCardIndex(value, suit, cards) {
     return cards.findIndex((card) => card.value === value && card.suit === suit);
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GameProvider } from './GameContex/GameContex';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <GameProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GameProvider>,
   document.getElementById('root')
 );
 
