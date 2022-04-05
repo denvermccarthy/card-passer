7a013f1851d3a8c4c7dbaa371fbc95fe65953be0
Denver McCarthy <dmichaelspdx@gmail.com>
2022-04-04 17:19:00 -0700

    wrapped in gameprovider

--- src/App.js
+++ src/App.js
@@ -5,69 +5,18 @@ import ExecutePassButton from './components/ExecutePassButton';
 import { useGameData } from './GameContex/GameContex';
 
 function App() {
-  const {
-    playerOneHand,
-    playerThreeHand,
-    playerTwoHand,
-    deck,
-    to,
-    setFrom,
-    selectedCard,
-    setTo,
-    setSelectedCard,
-    from,
-  } = useGameData();
+  const { playerOneHand, playerThreeHand, playerTwoHand, deck, selectedCard } = useGameData();
 
   return (
     <div className="App">
       <section>
         {/* if the player names are numbers, that will make our life easier later because we can reuse numbers as arrays. Note that this will make our app brittle! */}
-        <Player
-          to={to}
-          player={1}
-          hand={playerOneHand}
-          setFrom={setFrom}
-          selectedCard={selectedCard}
-          setTo={setTo}
-          setSelectedCard={setSelectedCard}
-        />
-        <Player
-          to={to}
-          player={2}
-          hand={playerTwoHand}
-          setFrom={setFrom}
-          selectedCard={selectedCard}
-          setTo={setTo}
-          setSelectedCard={setSelectedCard}
-        />
-        <Player
-          to={to}
-          player={3}
-          hand={playerThreeHand}
-          setFrom={setFrom}
-          selectedCard={selectedCard}
-          setTo={setTo}
-          setSelectedCard={setSelectedCard}
-        />
-        <CardList
-          cards={deck}
-          selectedCard={selectedCard}
-          setSelectedCard={setSelectedCard}
-          setFrom={setFrom}
-          player={'deck'}
-        />
+        <Player player={1} hand={playerOneHand} />
+        <Player player={2} hand={playerTwoHand} />
+        <Player player={3} hand={playerThreeHand} />
+        <CardList cards={deck} player={'deck'} />
       </section>
-      <section>
-        {selectedCard && (
-          <ExecutePassButton
-            setFrom={setFrom}
-            from={from}
-            to={to}
-            selectedCard={selectedCard}
-            setSelectedCard={setSelectedCard}
-          />
-        )}
-      </section>
+      <section>{selectedCard && <ExecutePassButton />}</section>
     </div>
   );
 }
